52cf52b91e8a108b0322145a6a64e14c53cb77e7
CampoSuarezJavierDavid <campo.javier1502@gmail.com>
2023-07-31 07:28:08 -0500

    feat: :sparkles: add dbContext

---
+++ Persistence/ShopContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+public class ShopContext : DbContext
+{
+    public ShopContext(DbContextOptions options) : base(options)
+    {}
+    public DbSet<Marca> Marcas { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Persona> Personas { get; set; }
+    public DbSet<PersonaProducto> PersonaProductos { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Provincia> Provincias { get; set; }
+    public DbSet<Region> Regiones { get; set; }
+    public DbSet<TipoPersona> TipoPersonas { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PersonaProducto>().HasKey(r => new{r.IdPersona,r.IdProducto});
+        base.OnModelCreating(modelBuilder);
+    }
+}
